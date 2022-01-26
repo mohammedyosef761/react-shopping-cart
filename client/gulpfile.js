595de0b155cfdf2c2cf36a41a38b30db0d3fa549
mohammedyosef761 <mohammedyosef712@gmail.com>
2022-01-27 01:27:50 +0200

    products comp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -8,7 +8,7 @@ const sass = gulpSass(require('sass'));
 // })
 
 gulp.task('watch',async function(){
-  gulp.watch('src/components/**/*.scss',function(){
+  gulp.watch('src/components/**/*.scss', async function(){
       gulp.src('src/components/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
   })
 })
