c388b0ac190e176d7be577c4d77061028f9cf373
mohammedyosef761 <mohammedyosef712@gmail.com>
2022-01-28 13:20:47 +0200

    animation

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "../../css/Products/Products.css"
 import {useState}from 'react';
 import ProductModal from './ProductModal';
-
+import Bounce from 'react-reveal/Bounce';
 
 const Products = (props) => {
    
@@ -17,23 +17,26 @@ const Products = (props) => {
         setProduct(false);
     } 
   return (
-    <div className="products-wrapper">
-    {props.products.map(product=>(
-        <div className='products-item' key={product.id} >
-                  <a href="#" onClick = { ()=>openModal(product)} >
-                     <img src={product.imageUrl} alt={product.title}/>
-                  </a>
-                    <div className='product-desc'>
-                       <p>{product.title}</p>
-                       <span>${product.price}</span>
-               </div>
-               <button onClick={()=>props.addToCart(product)}>Add to Cart</button>
-        </div>
-    ))}
+    <Bounce left cascade>
+              <div className="products-wrapper">
+              {props.products.map(product=>(
+                  <div className='products-item' key={product.id} >
+                            <a href="#" onClick = { ()=>openModal(product)} >
+                              <img src={product.imageUrl} alt={product.title}/>
+                            </a>
+                              <div className='product-desc'>
+                                <p>{product.title}</p>
+                                <span>${product.price}</span>
+                        </div>
+                        <button onClick={()=>props.addToCart(product)}>Add to Cart</button>
+                  </div>
+              ))}
 
 
-    <ProductModal  product={product}  closeModal={closeModal}/>
-    </div>
+              <ProductModal  product={product}  closeModal={closeModal}/>
+              </div>
+
+    </Bounce>
 
   );
 };
